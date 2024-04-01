ef8b79d00e0c0daf5e150095a100469f4c87f1ff
meriembenayad <meriem.benayad@gmail.com>
2024-04-01 13:53:56 +0000

    Task 2: Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -23,6 +23,20 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
+        return dataset[start_index: end_index]
+
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """
+        Calculate the start and end index for pagination.
+        Args:
+            - page (int): the current page number (1-indexed)
+            - page_size (int): the number of item per page
+        Return:
+            - tuple: A tuple contains the start & end idx for the page and size
+        """
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
@@ -48,7 +62,7 @@ class Server:
         if page > total_pages:
             return []
 
-        start_index, end_index = index_range(page, page_size)
+        start_index, end_index = self.index_range(page, page_size)
         return dataset[start_index: end_index]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
@@ -79,17 +93,3 @@ class Server:
             "prev_page": page - 1 if page < 1 else None,
             "total_page": total_pages
         }
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Calculate the start and end index for pagination.
-    Args:
-        - page (int): the current page number (1-indexed)
-        - page_size (int): the number of item per page
-    Return:
-        - tuple: A tuple containing the start & end index for the page and size
-    """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
