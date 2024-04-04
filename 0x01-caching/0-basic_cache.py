641c68e3b1e2d854352001818a2994c4d1b62be7
meriembenayad <meriem.benayad@gmail.com>
2024-04-04 00:05:26 +0000

    Task 0: Basic Dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" 0. Basic dictionary """
+import base_caching
+
+
+class BasicCache(base_caching.BaseCaching):
+    """ BasicCache class """
+
+    def put(self, key, item):
+        """ Add an iten in cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get item by key """
+        return self.cache_data.get(key, None)
