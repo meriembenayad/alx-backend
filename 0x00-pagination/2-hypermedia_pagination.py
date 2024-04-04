73bb1caefcb5456240f5f329cb3311c7b9bdd8ef
meriembenayad <meriem.benayad@gmail.com>
2024-04-04 16:47:08 +0000

    Task 2: Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -50,9 +50,18 @@ class Server:
         """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
+        data_set = self.dataset()
+
+        if page_size == 0:
+            return []
+
+        total_pages = math.ceil(len(data_set) / page_size)
+
+        if page > total_pages:
+            return []
 
         start_index, end_index = index_range(page, page_size)
-        return self.dataset()[start_index: end_index]
+        return data_set[start_index: end_index]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """
@@ -71,21 +80,14 @@ class Server:
                 'total_page': 'total_page'
             }
         """
+        data_page = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
 
-        # Ensure that the page doesn't exceed the total_page
-        if page > total_pages:
-            page = total_pages
-        elif page < 0:
-            page = 1
-
-        data_page = self.get_page(page, page_size)
-
         return {
-            'page_size': page_size,
+            'page_size': len(data_page),
             'page': page,
             'data': data_page,
-            'next_page': page + 1 if page < total_pages else None,
-            'prev_page': page - 1 if page > 1 else None,
+            'next_page': page + 1 if page + 1 <= total_pages else None,
+            'prev_page': page - 1 if page > 0 else None,
             'total_page': total_pages
         }
