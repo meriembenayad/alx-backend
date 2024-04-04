3de1a027fe7c45f120d854ae1e6ead55cc3c1763
meriembenayad <meriem.benayad@gmail.com>
2024-04-04 00:36:29 +0000

    Task 3: LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""" 3. LRU Caching """
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache class """
+
+    def __init__(self):
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """ Add item in cache using LRU caching """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.keys.remove(key)
+            elif len(self.keys) >= self.MAX_ITEMS:
+                discard = self.keys.pop(0)
+                self.cache_data.pop(discard)
+                print(f'DISCARD: {discard}')
+            self.cache_data[key] = item
+            self.keys.append(key)
+
+    def get(self, key):
+        """ Get item by key """
+        if key is not None and key in self.cache_data:
+            self.keys.remove(key)
+            self.keys.append(key)
+        return self.cache_data.get(key, None)
