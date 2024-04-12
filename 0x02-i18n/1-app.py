d0fabd3ea96deee87dd7904bc543b508b6ed31b6
meriembenayad <meriem.benayad@gmail.com>
2024-04-12 22:17:39 +0000

    Task 1: Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""  """
+""" 1. Basic Babel setup """
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config(object):
-    """  """
+    """ Configuration Babel """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -18,7 +18,7 @@ babel = Babel(app)
 
 @app.route('/')
 def index():
-    """  """
+    """ render template """
     return render_template('1-index.html')
 
 
