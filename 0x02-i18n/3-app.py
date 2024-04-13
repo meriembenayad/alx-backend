31a44f35b107d68394261533e07b44f321d936b3
meriembenayad <meriem.benayad@gmail.com>
2024-04-13 01:02:39 +0000

    Task 3: Parameterize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -11,12 +11,6 @@ class Config(object):
     """ BABEL_DEFAULT_TIMEZONE = "UTC" """
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app)
-
-
-@babel.localeselector
 def get_locale():
     """
         function with the babel.localeselector decorator.
@@ -24,6 +18,11 @@ def get_locale():
         match with our supported languages.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
