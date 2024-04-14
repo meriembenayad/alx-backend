48a8e20f1ef00c5fb23c5bf4d4eed677f8f1857e
meriembenayad <meriem.benayad@gmail.com>
2024-04-15 00:10:34 +0100

    Task 0: Basic Flask app (style fixed)

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""  """
+""" 0. Basic Flask app """
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    """  """
+    """ render template """
     return render_template('0-index.html')
 
 
