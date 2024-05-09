ce1837cfa5085ef75d321039e1937df2b8301b71
meriembenayad <meriem.benayad@gmail.com>
2024-05-09 00:56:35 +0000

    TAsk 2: Node Redis Client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+import redis from 'redis'
+
+const client = redis.createClient();
+
+client.on('connect', function() {
+	console.log('Redis client connected to the server');
+});
+
+client.on('error', function(err) {
+	console.log(`Redis client not connected to the server: ${err}`);
+});
